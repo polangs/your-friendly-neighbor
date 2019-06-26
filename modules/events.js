807021170e8ca1a8c476772120de2ee670e89c14
Matthew_Heyert <heyerthb@gmail.com>
2019-06-26 08:36:05 -0700

    created a schema, did some work with intuitive naming, set up sql call in js, created table with sample data

--- modules/events.js
+++ modules/events.js
@@ -1,16 +1,16 @@
 function getEvents(query, superagent){
-    console.log('query', query);
-    const URL = `https://www.eventbriteapi.com/v3/events/search?location.address=${query.formatted_query}&location.within=1km`
-    return superagent.get(URL)
-      .set('Authorization', `Bearer ${process.env.EVENT_BRITE}`)
-      .then(data => data.body.events.map(event => new Event(event)));
-  }
-  
-  function Event(event){
-    this.link = event.url,
-    this.name= event.name.text,
-    this.event_date = event.start.local,
-    this.summary = event.summary
-  }
+  console.log('query', query);
+  const URL = `https://www.eventbriteapi.com/v3/events/search?location.address=${query.formatted_query}&location.within=1km`
+  return superagent.get(URL)
+    .set('Authorization', `Bearer ${process.env.EVENT_BRITE}`)
+    .then(data => data.body.events.map(event => new Event(event)));
+}
 
-  module.exports = getEvents;
+function Event(event){
+  this.link = event.url,
+  this.event_name= event.name.text, //changed this.name to this.event_name
+  this.event_date = event.start.local,
+  this.summary = event.summary
+}
+
+module.exports = getEvents;
